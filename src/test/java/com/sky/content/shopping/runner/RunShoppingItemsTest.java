96cd3870ded27b821f406b627790f2e4be0647f7
vp8791 <neetha.po@gmail.com>
2018-11-06 09:37:44 +0000

    All Tests

--- src/test/java/com/sky/content/shopping/runner/RunShoppingItemsTest.java
+++ src/test/java/com/sky/content/shopping/runner/RunShoppingItemsTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         format = { "pretty", "html:target/cucumber-html" , "json:target/cucumber-report-json.json"},
-        glue = {"com.sky.content.shopping.shoppingitems", "com.mykyong.bdd" }
+        glue = {"com.sky.content.shopping.shoppingitems.steps", "com.mykyong.bdd.steps" }
 )
 public class RunShoppingItemsTest {
 }
